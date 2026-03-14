772945f298ecd568d3c180ce1d69d10d4b33c496
maedayosimasa <gdre131@yahoo.co.jp>
2026-03-14 17:48:43 +0900

    項目追加

--- Src/ExampleDialog.cpp
+++ Src/ExampleDialog.cpp
@@ -1,35 +1,77 @@
 ﻿#include "ExampleDialog.hpp"
 #include "ACAPinc.h"
+#include "ResourceIds.hpp"
 
 ExampleDialog::ExampleDialog () :
+    // リソースモジュールを明示的に指定
     DG::ModalDialog (ACAPI_GetOwnResModule (), ID_ADDON_DLG, ACAPI_GetOwnResModule ()),
-    searchButton (GetReference (), 4),
-    treeView     (GetReference (), 5),
-    listBox      (GetReference (), 6)
+    searchButton (GetReference (), SearchButtonId),
+    messageLabel (GetReference (), LabelId),
+    wallCheckBox (GetReference (), WallCheckId),
+    columnCheckBox (GetReference (), ColumnCheckId),
+    beamCheckBox (GetReference (), BeamCheckId),
+    slabCheckBox (GetReference (), SlabCheckId),
+    roofCheckBox (GetReference (), RoofCheckId),
+    objectCheckBox (GetReference (), ObjectCheckId),
+    windowCheckBox (GetReference (), WindowCheckId),
+    doorCheckBox (GetReference (), DoorCheckId)
 {
-    Attach (*this);
+    // 検索ボタンのみオブザーバーを接続
     searchButton.Attach (*this);
-
-    // ツリーの初期化
-    Int32 rootId = treeView.AppendItem (DG::TreeView::RootItem);
-    treeView.SetItemText (rootId, "01_Archicad Objects");
     
-    Int32 subId = treeView.AppendItem (rootId);
-    treeView.SetItemText (subId, "Furniture Category");
-    
-    treeView.ExpandItem (rootId);
+    // デフォルトのチェック状態を設定
+    wallCheckBox.Check ();
+    slabCheckBox.Check ();
 }
 
 ExampleDialog::~ExampleDialog ()
 {
+    // 確実にデタッチ
     searchButton.Detach (*this);
-    Detach (*this);
+}
+
+// カウント処理の修正
+USize ExampleDialog::CountElements (const API_ElemTypeID& typeID)
+{
+    GS::Array<API_Guid> guids;
+    // Archicad 26以降、API_ElemTypeID をそのまま渡す形式でOK（API_ElemType 構造体を使用する場合もあり）
+    GSErrCode err = ACAPI_Element_GetElemList (typeID, &guids);
+    if (err == NoError) {
+        return guids.GetSize ();
+    }
+    return 0;
 }
 
 void ExampleDialog::ButtonClicked (const DG::ButtonClickEvent& ev)
 {
     if (ev.GetSource () == &searchButton) {
-        short newItemId = listBox.AppendItem ();
-        listBox.SetTabItemText (newItemId, 1, "Searching...");
+        GS::UniString resultText = "--- Search Results ---\n";
+        USize totalCount = 0;
+
+        // ラムダ関数の引数型を API_ElemTypeID に最適化
+        auto AddToResult = [&](const char* name, bool isChecked, API_ElemTypeID type) {
+            if (isChecked) {
+                USize count = CountElements (type);
+                // %u ではなく GS::UniString::Printf の型安全な指定を使用
+                resultText += GS::UniString::Printf ("%s: %u\n", name, (unsigned int)count);
+                totalCount += count;
+            }
+        };
+
+        // 各要素の集計
+        AddToResult ("Walls",   wallCheckBox.IsChecked (), API_WallID);
+        AddToResult ("Columns", columnCheckBox.IsChecked (), API_ColumnID);
+        AddToResult ("Beams",   beamCheckBox.IsChecked (), API_BeamID);
+        AddToResult ("Slabs",   slabCheckBox.IsChecked (), API_SlabID);
+        AddToResult ("Roofs",   roofCheckBox.IsChecked (), API_RoofID);
+        AddToResult ("Objects", objectCheckBox.IsChecked (), API_ObjectID);
+        AddToResult ("Windows", windowCheckBox.IsChecked (), API_WindowID);
+        AddToResult ("Doors",   doorCheckBox.IsChecked (), API_DoorID);
+
+        resultText += "----------------------\n";
+        resultText += GS::UniString::Printf ("Total: %u items", (unsigned int)totalCount);
+        
+        // 結果を表示
+        messageLabel.SetText (resultText);
     }
 }
--- Src/ExampleDialog.hpp
+++ Src/ExampleDialog.hpp
@@ -1,20 +1,42 @@
-﻿#ifndef EXAMPLE_DIALOG_HPP
-#define EXAMPLE_DIALOG_HPP
+﻿#ifndef EXAMPLEDIALOG_HPP
+#define EXAMPLEDIALOG_HPP
 
 #include "DGModule.hpp"
-#include "DGTreeView.hpp"
-#include "DGListBox.hpp"
-#include "ResourceIds.hpp"
+#include "ACAPinc.h" // API_ElemTypeID のために必要
 
-// 設計図：ここには「何があるか」だけを書く
 class ExampleDialog : public DG::ModalDialog,
                       public DG::PanelObserver,
-                      public DG::ButtonItemObserver
+                      public DG::ButtonItemObserver,
+                      public DG::CompoundItemObserver
 {
 private:
-    DG::Button              searchButton;
-    DG::SingleSelTreeView   treeView;     // [5]
-    DG::SingleSelListBox    listBox;      // [6]
+    enum {
+        DialogId = 32502,
+        SearchButtonId = 1,
+        LabelId = 2,
+        WallCheckId = 3,
+        ColumnCheckId = 4,
+        BeamCheckId = 5,
+        SlabCheckId = 6,
+        RoofCheckId = 7,
+        ObjectCheckId = 8,
+        WindowCheckId = 9,
+        DoorCheckId = 10
+    };
+
+    DG::Button      searchButton;
+    DG::LeftText    messageLabel;
+    DG::CheckBox    wallCheckBox;
+    DG::CheckBox    columnCheckBox;
+    DG::CheckBox    beamCheckBox;
+    DG::CheckBox    slabCheckBox;
+    DG::CheckBox    roofCheckBox;
+    DG::CheckBox    objectCheckBox;
+    DG::CheckBox    windowCheckBox;
+    DG::CheckBox    doorCheckBox;
+
+    // ここを修正：引数に const と & を付ける
+  USize CountElements (const API_ElemTypeID& typeID);
 
 public:
     ExampleDialog ();
